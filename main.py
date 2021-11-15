113e43e26ad6074030f3c5978ff773705d3f6f25
kittiwakekt <kittiwakekt@gmail.com>
2021-11-15 19:57:25 +0300

    v1

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import kivy
 from kivy.app import App
+from kivy.core import text
+from kivy.uix.boxlayout import BoxLayout
+# WARNING: The scripts buildozer and buildozer-remote are installed in '/home/kittiwake/.local/bin' which is not on PATH.
+# Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
 
+class MainWidjet(BoxLayout):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return MainWidjet()
+
+MainApp().run()
